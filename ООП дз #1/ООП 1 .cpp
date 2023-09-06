ca897150a950cfcfdd4429c8a339e2bdd71d4be8
denua <denua@DESKTOP-350U2OD>
2023-09-06 08:15:31 +0300

    Add project files.

---
+++ ООП дз #1/ООП 1 .cpp
@@ -0,0 +1,156 @@
+#include <iostream>
+using namespace std;
+
+
+class man {// Мужчина #1
+
+	double man_height = 180;
+	int man_eyes = 2;
+	string name = "Viktor";
+	string eyes_color = "green";
+	double man_weight = 78;
+
+	void MakeNoize() {
+		cout << "Hello\n";
+	}
+	void Walk() {
+		cout << "Man Running\n";
+	}
+	void Sleep() {
+		cout << "Good Night!\n";
+	}
+	void Happy() {
+		cout << "Listening to music\n";
+	}
+	void Cleaning() {
+		cout << "Turn On Washing maсhine\n";
+	}
+};
+
+class phone {// Телефон #2
+
+	double phone_height = 22;
+	int    camera = 4;
+	string name = "Samsung";
+	string phone_color = "Black";
+	double phone_weight = 187;
+
+	void MakeNoize() {
+		cout << "Ok Google              \n";
+	}
+	void call() {
+		cout << "Incoming call          \n";
+	}
+	void  charge() {
+		cout << "Charge 62%             \n";
+	}
+	void AlarmClock() {
+		cout << "Vibration              \n";
+	}
+	void Crash() {
+		cout << " Reboot                \n";
+	}
+};
+
+
+class Room {// Комната #3
+
+	double square_meters = 27;
+	int number_of_people = 3;
+	string City = "Odessa";
+	int window = 2;
+	string Stuffed_Toysnam_weight = "Big Taddy";
+
+	void MakeNoize() {
+		cout << "Hello\n";
+	}
+	void Walk() {
+		cout << "Man Running\n";
+	}
+	void Sleep() {
+		cout << "Good Night!\n";
+	}
+	void Happy() {
+		cout << "Listening to music\n";
+	}
+	void Cleaning() {
+		cout << "Turn On Washing maсhine\n";
+	}
+};
+
+
+class pаrrot {  // Попугай #4
+
+	string parrot_color = "red-blue-yellow";
+	string parrot_name = "Kesha";
+	int eyes = 2;
+	int wings = 2;
+	int beak = 1;
+
+
+	void  MakeNoize() {
+		cout << " Caaarr  Caaarr"\n";
+	}
+
+	void  talk() {
+		cout << " Answer\n";
+	}
+
+	void eyes() {
+		cout << "black\n";
+	}
+
+	void fruits interior() {
+		cout << " eat \n";
+	}
+
+	void foots() {
+		cout << " going \n";
+	}
+}
+
+class car {  // машина #5
+
+	string car_color = "black";
+	string car_name = "Rols Roys";
+	int   window = 6;
+	double car_prise = 400, 000;
+	int steering_wheel = 1;
+
+
+	void  MakeNoize() {
+		cout << "Vrumvrum\n";
+	}
+
+	void  ride() {
+		cout << "280\n";
+	}
+
+	void lights() {
+		cout << "White\n";
+	}
+
+	void car interior() {
+		cout << "Black-Brown\n";
+	}
+
+	void whell() {
+		cout << " 4 \n";
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+
+}
